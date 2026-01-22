9a08f9c37fd255ba1f016f132029a1e7de9754e0
BrenoAcker <brenoack@amazon.com>
2026-01-22 13:17:51 -0300

    Adding test to search an existing order

--- playwright/e2e/tests/orders.spec.ts
+++ playwright/e2e/tests/orders.spec.ts
@@ -16,3 +16,21 @@ test('Search for a non-existing order', async ({ page }) => {
   await expect(page.getByText('Verifique o número do pedido')).toBeVisible();
 });
 
+test('Search for an existing order', async ({ page }) => {
+  // Arrange
+  await page.goto('http://localhost:5173/');
+  await expect(page.getByTestId('hero-section').getByRole('heading', { name: 'Velô Sprint' })).toBeVisible();
+
+  // Act
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+  await page.getByTestId('search-order-id').click();
+  await page.getByTestId('search-order-id').fill('VLO-6AWB2D');
+  await page.getByTestId('search-order-button').click();
+  
+  // Assert
+  await expect(page.getByTestId('order-result-id')).toBeVisible();
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-6AWB2D');
+  await expect(page.getByTestId('order-result-status')).toBeVisible();
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+});
+
