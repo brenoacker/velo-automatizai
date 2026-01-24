dd2c0f477f60c6fd1b70a4ccf1fb448a4204f76b
BrenoAcker <brenoack@amazon.com>
2026-01-24 10:32:33 -0300

    Changing the tests to not use data-testid locator

--- playwright/e2e/tests/orders.spec.ts
+++ playwright/e2e/tests/orders.spec.ts
@@ -1,36 +1,44 @@
 import { test, expect } from '@playwright/test';
 
-test('Search for a non-existing order', async ({ page }) => {
-  // Arrange
-  await page.goto('http://localhost:5173/');
-  await expect(page.getByTestId('hero-section').getByRole('heading', { name: 'Velô Sprint' })).toBeVisible();
-
-  // Act
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-  await page.getByTestId('search-order-id').click();
-  await page.getByTestId('search-order-id').fill('abcd');
-  await page.getByTestId('search-order-button').click();
-  
-  // Assert
-  await expect(page.getByRole('heading', { name: 'Pedido não encontrado' })).toBeVisible();
-  await expect(page.getByText('Verifique o número do pedido')).toBeVisible();
-});
+test.beforeEach(async ({ page }) => {
+  await page.goto('/');
+})
 
 test('Search for an existing order', async ({ page }) => {
   // Arrange
-  await page.goto('http://localhost:5173/');
-  await expect(page.getByTestId('hero-section').getByRole('heading', { name: 'Velô Sprint' })).toBeVisible();
+  const existingOrderId = 'VLO-6AWB2D'
+
+  await expect(page.getByRole('heading', { name: 'Velô Sprint', exact: true })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Consultar Pedido' })).toBeVisible();
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
 
   // Act
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-  await page.getByTestId('search-order-id').click();
-  await page.getByTestId('search-order-id').fill('VLO-6AWB2D');
-  await page.getByTestId('search-order-button').click();
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).click()
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(existingOrderId);
+  await expect(page.getByRole('button', {name: 'Buscar Pedido'})).toBeVisible()
+  await page.getByRole('button', {name: 'Buscar Pedido'}).click();
   
   // Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible();
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-6AWB2D');
-  await expect(page.getByTestId('order-result-status')).toBeVisible();
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+  await expect(page.getByText('Pedido', { exact: true })).toBeVisible({timeout: 10_000});
+  await expect(page.getByText(existingOrderId)).toBeVisible();
+  await expect(page.getByText('APROVADO')).toBeVisible();
 });
 
+test('Search for a non-existing order', async ({ page }) => {
+  // Arrange
+  const nonExistingOrderId = 'abcd';
+
+  await expect(page.getByRole('heading', { name: 'Velô Sprint', exact: true })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Consultar Pedido' })).toBeVisible();
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+
+  // Act
+  await expect(page.getByRole('textbox', { name: 'Número do Pedido' })).toBeVisible();
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).click();
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(nonExistingOrderId);
+  await page.getByRole('button', {name: 'Buscar Pedido'}).click();
+  
+  // Assert
+  await expect(page.getByRole('heading', { name: 'Pedido não encontrado' })).toBeVisible({timeout: 10_000});
+  await expect(page.getByText('Verifique o número do pedido')).toBeVisible();
+});
